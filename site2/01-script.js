0a7effa85e265ddb97d3a54020add26b3b3fdfdb
ManinhoGTP <gabrieltuler99@gmail.com>
2026-06-09 21:18:02 -0300

    +

--- site2/01-script.js
+++ site2/01-script.js
@@ -13,3 +13,15 @@ function fazerLogin(){
     }
     
 }
+function mostrarSection(sectionId){
+
+    const section = document.querySelectorAll(".tela")
+    
+    section.forEach(section => {
+        section.classList.remove("ativa")
+    })
+    
+    const sectionAtiva = document.getElementById(sectionId)
+    sectionAtiva.classList.add("ativa")
+
+}
